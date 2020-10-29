81b64e3ba8ceefdb8eea764bcd3234dda3da090f
Maja Gedda <maja.gedda@zocom.se>
2020-10-29 09:49:39 +0100

    init repo

---
+++ script.js
@@ -0,0 +1,29 @@
+//Del 1
+//Skriv en funktion för att kolla om datumet är giltigt eller ej
+
+//Del 2
+//Skriv en funktion för att få ut dagens datum
+
+//Del 3
+//Skriv en funktion för att få ut numret på månaden
+
+//Del 4
+//Skriv en funktion för att få ut månadens namn från en lista
+
+//Del 5
+//Skriv en funktion som jämför två datum, alltså kollar om ett datum är senare än det första
+
+//Del 6
+//Skriv en funktion som lägger till några extra minuter på ett datum
+
+//Del 7
+//Skriv en funktion som kollar om dagens datum är helg (lör/sön)
+
+//Del 8
+//Skriv en funktion som kollar tidsspannet mellan två datum
+
+//Del 9
+//Skriv en funktion som returnerar sista dagen på en månad
+
+//Del 10
+//Skriv en funktion som får ut gårdagens datum
