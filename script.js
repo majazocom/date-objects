15c9efee2d8d0904bfc2f364fd734d6799d4249b
Maja Gedda <maja.gedda@zocom.se>
2020-10-29 10:52:05 +0100

    new exercise

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
 //Del 1
-//Skriv en funktion för att kolla om datumet är giltigt eller ej
+//Skriv en funktion för att få ut dagens datum
+let currentDate = () => {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear().toString(); //O use toString();
+    
+
+    return (`${dd}-${mm}-${yyyy}`)
+}
+
+console.log(currentDate());
 
 //Del 2
-//Skriv en funktion för att få ut dagens datum
-
-//Del 3
 //Skriv en funktion för att få ut numret på månaden
 
-//Del 4
+let currentMonth = () => {
+    let today = new Date();
+    let mm = today.getMonth() + 1;
+
+    return `${mm}`
+}
+
+console.log(currentMonth());
+
+//Del 2.5
+//Skriv en funktion för att få ut antalet dagar i månaden
+
+let daysInMonth = (month,year) => {
+    
+
+    //Vi vill ha ut antalet dagar
+}
+
+//Del 3
 //Skriv en funktion för att få ut månadens namn från en lista
 
-//Del 5
+//Del 4
 //Skriv en funktion som jämför två datum, alltså kollar om ett datum är senare än det första
 
-//Del 6
+//Del 5
 //Skriv en funktion som lägger till några extra minuter på ett datum
 
-//Del 7
+//Del 6
 //Skriv en funktion som kollar om dagens datum är helg (lör/sön)
 
-//Del 8
+//Del 7
 //Skriv en funktion som kollar tidsspannet mellan två datum
 
-//Del 9
+//Del 8
 //Skriv en funktion som returnerar sista dagen på en månad
 
-//Del 10
+//Del 9
 //Skriv en funktion som får ut gårdagens datum
+
+//Del 10
+//Skriv en funktion för att kolla om datumet är giltigt eller ej
